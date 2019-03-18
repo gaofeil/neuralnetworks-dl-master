a3b1b971c943167f81419284d00c23bd0e39b9e6
gaofei <gaofei_li0@163.com>
2019-03-18 11:37:21 +0800

    first commit network

---
+++ fig/test.py
@@ -0,0 +1,7 @@
+import mnist_loader
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
