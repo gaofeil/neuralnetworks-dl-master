76f8dca6fb5a81bb79c58a4ed38d02c4325bc7d0
gaofei <gaofei_li0@163.com>
2019-03-22 15:04:19 +0800

    add network2

--- fig/test.py
+++ fig/test.py
@@ -1,3 +1,4 @@
+"""
 import mnist_loader
 import network
 
@@ -5,3 +6,14 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+"""
+
+
+#加入交叉熵
+import mnist_loader
+import network2
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network2.Network([784, 30, 10],cost = network2.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,monitor_evaluation_accuracy=True)
