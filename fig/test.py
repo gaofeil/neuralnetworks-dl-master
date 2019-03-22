c6afbaf97173fc776aa37a6ebb94ddf9820d8600
gaofei <gaofei_li0@163.com>
2019-03-22 16:43:33 +0800

    add new chushihua test

--- fig/test.py
+++ fig/test.py
@@ -1,4 +1,5 @@
 """
+测试network
 import mnist_loader
 import network
 
@@ -8,7 +9,7 @@ net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 """
 
-
+'''
 #加入交叉熵
 import mnist_loader
 import network2
@@ -17,3 +18,12 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = network2.Network([784, 30, 10],cost = network2.CrossEntropyCost)
 net.large_weight_initializer()
 net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,monitor_evaluation_accuracy=True)
+'''
+
+import mnist_loader
+import network2
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network2.Network([784, 30, 10],cost = network2.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(training_data, 30, 10, 0.1, lmbda=5.0, evaluation_data=test_data,monitor_evaluation_accuracy=True)
